dbcfdf6b89997a8cc90a7b1c0327215dc53efb3a
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-08-17 14:06:54 +0300

    feat: create EntityManager

---
+++ src/main/java/dao/util/PersistUtil.java
@@ -0,0 +1,14 @@
+package dao.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class PersistUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("weatherPersistence");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+}
