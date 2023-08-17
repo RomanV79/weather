3fa0a4deec4cf95c02d3b9bca0529c7be7ce7963
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-08-17 14:09:56 +0300

    feat: create UserDao

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,25 @@
 package dao;
 
+import dao.util.PersistUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import lombok.extern.slf4j.Slf4j;
+import model.User;
+
+@Slf4j
 public class UserDao {
 
+    private final EntityManager entityManager = PersistUtil.getEntityManager();
+
+    public void insert (User user) {
+        log.info("Start save user -> {}", user);
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.persist(user);
+        entityManager.flush();
+
+        transaction.commit();
+        log.info("Success insert user");
+    }
 }
