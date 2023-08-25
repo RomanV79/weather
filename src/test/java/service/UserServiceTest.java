ebaddac88787533acb40a17621b57664e2b14cf4
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-08-25 17:15:13 +0300

    ref: create Config class for TestContainer

---
+++ src/test/java/service/UserServiceTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import CustomException.UserExistException;
+import CustomException.UserNotFoundException;
+import dao.UserDao;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class UserServiceTest extends TestContainerConfig {
+
+    @Test
+    void user_insert_get_UserNotFoundException_UserExistException() throws UserExistException, UserNotFoundException {
+        EntityManager entityManager = emf.createEntityManager();
+        UserDao userDao = new UserDao(entityManager);
+        UserService userService = new UserService(userDao);
+
+        String login = "Bob";
+
+        // try to get a user that doesn't  exist in DB
+        assertThrows(UserNotFoundException.class, () -> userService.getByLogin(login));
+
+        // try to insert user in DB and get them after
+        userService.insert(login, "12345");
+        assertEquals(login, userService.getByLogin(login).getLogin());
+
+        // try to insert user with existing login
+        assertThrows(UserExistException.class, () -> userService.insert(login, "12345"));
+    }
+}
