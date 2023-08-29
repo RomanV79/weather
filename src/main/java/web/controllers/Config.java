7387f61778e554e5e280c45fafb81d09a4493683
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-08-29 08:06:21 +0300

    feat: create First migration by FlyWay when application start

---
+++ src/main/java/web/controllers/Config.java
@@ -0,0 +1,36 @@
+package web.controllers;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@WebListener()
+@Slf4j
+public class Config implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent event) {
+
+        String flywayNameProp = "flyway.properties";
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(flywayNameProp);
+        Properties properties = new Properties();
+
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = properties.getProperty("flyway.url");
+        String user = properties.getProperty("flyway.user");
+        String password = properties.getProperty("flyway.password");
+
+        Flyway flyway = Flyway.configure().dataSource(url, user, password).load();
+        flyway.migrate();
+    }
+}
